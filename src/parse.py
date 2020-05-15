4fbb6b220160544687043a326b25042264a138bd
Otávio Augusto <otavioaraujo@dcc.ufba.br>
2020-05-15 14:06:21 -0300

    Update parse.py

--- src/parse.py
+++ src/parse.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import os
+import logging
+import stat
 
 from typing import Optional
 from xml.etree import ElementTree as ET
@@ -8,26 +10,21 @@ from argparse import ArgumentParser
 from collections import namedtuple
 from sklearn.utils import shuffle
 
-ROOT_NAME = "assin-pt{0}-"
+FILES_LOCATION = 'dataset/xml'
 
-# FILE_FOR_TEST = 'dataset/xml/read_test.xml'
-FILES_LOCATION = '../dataset/xml/{0}/{1}'
+MODES = namedtuple('MODER', ['r', 'w'])
 
-FOLDERS = namedtuple('FOLDERS', ['eu', 'br', 'eubr'])
+MODES = MODES(r=stat.S_IREAD, w=stat.S_IWRITE)
 
-FOLDERS = FOLDERS(
-    eu=FILES_LOCATION.format("EU", ROOT_NAME.format("pt")),
-    br=FILES_LOCATION.format("BR", ROOT_NAME.format("br")),
-    eubr = FILES_LOCATION.format("EUBR", ROOT_NAME.format("eubr"))
-)
-
+logger = logging.getLogger()
 
 def parse_xml(file_path):
-    
+
+
+    file_path = '/'.join([FILES_LOCATION, file_path])
+    logger.info("Parsing xml file")
     tree = ET.parse(file_path)
-
     root = tree.getroot()
-
     info_pares_sentencas = list()
 
     for child in root:
@@ -38,51 +35,45 @@ def parse_xml(file_path):
 
         info_pares_sentencas.append(d)
 
-
     return info_pares_sentencas
 
-
-
-def create_df(L):
+def create_df(L: list):
+    logging.info("Creating dataframe")
     df = pd.DataFrame(L)
-
     df = df.rename(columns={"entailment":"Relacao", "similarity": "Similaridade"})
-
     #df["Implicação"] = df.Relação.map({"Entailment": "Sim", "None": "Não"})
 
+    logging.info("Dataframe created.")
+    
     return df
 
 
-def save_df(df: pd.DataFrame, file_path: str):
-
+def save_as_csv(df: pd.DataFrame, file_path: str):
+    logging.info(f"Saving dataframe in {file_path}")
     file_path = file_path.replace('xml', 'csv')
+
+    if 'csv' not in os.listdir('dataset'):
+      #  os.chmod(r'dataset/csv', mode=MODES.w)
+        os.mkdir(r'dataset/csv')
 
     with open(file_path, mode='w+', encoding='utf-8', newline="") as f:
         df.to_csv(f, index=False)
 
+    # logging.info("Dataframe saved.")
 
 def build_dataset(args: Optional[ArgumentParser]=None):
-    brpt_dataset = parse_xml(FOLDERS.br) 
-  
-    if args.j:
-        eupt_dataset = parse_xml(FOLDERS.eu)      
-        corpus = brpt_dataset + eupt_dataset
 
-
-    corpus = shuffle(np.array(corpus))
-    corpus = create_df(corpus)
-
-    if args.s:
-        return save_df(corpus, FOLDERS)
-
-    return corpus
-
+    files_name = os.listdir(FILES_LOCATION)
+    for f in files_name:
+        parsed_file = parse_xml(f)
+        df = create_df(parsed_file)
+        save_as_csv(df, os.path.join(FILES_LOCATION, f))
 
 def main(args):
     build_dataset(args)
 
-    print("FIM.")
-    
+    logger.info("FIM.")
+
 if __name__ == "__main__":
     
     parser = ArgumentParser()
